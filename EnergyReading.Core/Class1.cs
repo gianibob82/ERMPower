37f79c54a0e41a24a6685225b77dc6b7e2086b1e
Roberto Gianisella <gianibob82@gmail.com>
2018-08-11 09:42:39 +1000

    solution setup

---
+++ EnergyReading.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EnergyReading.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ EnergyReading.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EnergyReading.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
